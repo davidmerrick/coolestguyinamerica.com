1fd74c733f0ea2717ce09ace5d25366100e2bc9c
David Merrick <david@david-merrick.com>
2019-03-31 09:32:11 -0700

    Starting to flesh out content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Typography, AppBar, Toolbar, Grid } from "@material-ui/core";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h4" color="inherit">
+              Coolest Guy in America: David Merrick
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Grid container>
+          <Grid item xs="12">
+            <Typography variant="h4" color="inherit">
+              David Merrick is the coolest guy in America
+            </Typography>
+            <div>
+              But David, how do you know you're the coolest guy in America?
+              <br />
+              It's simple. Because Google says so. And Google is never wrong!
+            </div>
+          </Grid>
+        </Grid>
       </div>
     );
   }
