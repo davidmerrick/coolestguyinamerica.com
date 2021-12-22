d2ed51bba36ef112386e6adea5d8332c049a083f
David Merrick <david@david-merrick.com>
2021-12-22 13:13:15 -0800

    Cleaned up header text a bit. Added responsive breakpoints. Centered entire thing in a container. Added Adsense.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import {Grid} from "@mui/material";
+import {Container, Grid} from "@mui/material";
 import {BodyText} from "./BodyText";
 import Typography from "@mui/material/Typography";
 import ReactGA from 'react-ga';
+import AdSense from "react-adsense";
+
 ReactGA.initialize('UA-8004148-7');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 export function App() {
-    return <Grid container spacing={2}>
-        <Grid item xs={12}>
-            <Typography variant="h1">David Merrick is the Coolest Guy in America</Typography>
-        </Grid>
-        <Grid item xs={4}
-              justify="space-between"
-              style={{
-                  backgroundImage: `url(coolestguy.jpg)`,
-                  height: '800px',
-                  backgroundSize: 'cover'
-              }}
+  return <Container justify="center">
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Typography variant="h1" align="center">David Merrick</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="h1" align="center">is the</Typography>
+        <Typography variant="h1" align="center">Coolest Guy in America</Typography>
+      </Grid>
+      <Grid item xs={12} style={{ height: 50 }}/>
+      <Grid item
+            xs={12}
+            lg={6}
+            justify="space-between"
+            style={{
+              backgroundImage: `url(coolestguy.jpg)`,
+              backgroundSize: 'cover',
+              height: 500
+            }}
+      />
+      <Grid item xs={12} lg={6}>
+        <BodyText></BodyText>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <AdSense.Google
+          client='ca-pub-2926548784382786'
+          slot='5755823672'
+          style={{ display: 'block' }}
+          layout='in-article'
+          format='fluid'
         />
-        <Grid item xs={4}>
-            <BodyText></BodyText>
-        </Grid>
+      </Grid>
     </Grid>
+  </Container>
 }
